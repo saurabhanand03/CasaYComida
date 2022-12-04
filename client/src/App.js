6c3363a855ecc5e7f94d89e7a5117136ce589e4a
saurabhanand03 <asaurabh2003@gmail.com>
2022-12-03 20:48:55 -0500

    Fix Log In page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { SignUp } from './components/SignUp';
 import { Login } from './components/Login';
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react"
 
 // Login page
 export const Login = (props) => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     // Handles page states for entering and submitting login info
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(username);
+        console.log(email);
     }
     
     return (
         <div className="auth-form-container">
             <h2>Log In</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
-                <input value={username} onChange={(event) => setUsername(event.target.value)} type="username" placeholder="Enter Username" id="username" name="username"/>
+                <label htmlFor="email">Email</label>
+                <input value={email} onChange={(event) => setEmail(event.target.value)} type="email" placeholder="Enter Email" id="email" name="email"/>
                 <label htmlFor="password">Password</label>
                 <input value={password} onChange={(event) => setPassword(event.target.value)} type="password" placeholder="Enter Password" id="password" name="password"/>
                 <button className="link-btn" onClick={() => props.onFormSwitch('signup')}>Don't have an account? Sign up here</button>
