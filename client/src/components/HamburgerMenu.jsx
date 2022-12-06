9f6fa6f0f2894e968b153a960e2060302949c406
saurabhanand03 <asaurabh2003@gmail.com>
2022-12-06 04:41:43 -0500

    Modify home page to contain markers

--- client/src/components/HamburgerMenu.jsx
+++ client/src/components/HamburgerMenu.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function HamburgerMenu(){ 
   return <div className="hamburger-menu">
-    <p className="hamburger-menu-icon">menu</p>
+    {/* <p className="hamburger-menu-icon">menu</p> */}
   </div>;
 }
---
+++ client/src/markerLocations.js
@@ -0,0 +1,24 @@
+export const markerLocations = [
+    {
+        name: "location one",
+        locationtype: "Shelter",
+        description: "description",
+        lat: 29.631979,
+        lng: -82.305020
+    },
+    {
+        name: "location two",
+        locationtype: "Daycare",
+        description: "description",
+        lat: 29.641979,
+        lng: -82.315020
+    },
+    {
+        name: "location three",
+        locationtype: "Shelter",
+        description: "description",
+        lat: 29.651979,
+        lng: -82.325020
+    },
+];
+
