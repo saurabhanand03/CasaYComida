eb8ac7abba299de6c60ea3f9942fd58dec51b726
Alejandro Santana <asantana1@ufl.edu>
2022-12-06 17:39:39 -0500

    added comments to HamburgerMenu.jsx

--- client/src/components/HamburgerMenu.jsx
+++ client/src/components/HamburgerMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Contents of Hamburger Menu Icon
 export default function HamburgerMenu(){ 
   return <div className="hamburger-menu">
     {/* <p className="hamburger-menu-icon">menu</p> */}
